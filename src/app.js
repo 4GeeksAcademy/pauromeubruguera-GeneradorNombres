c0d5193d277465fe1c0b8f498fa03b9c3a329b33
Pau Romeu Bruguera <pau.romeu.bruguera@gmail.com>
2024-03-27 19:05:56 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,28 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let domain = [".com", ".net", ".us", ".io"];
+let insertText = "<ul class='list-group'>";
+
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  pronoun.map(item => {
+    adj.map(item2 => {
+      noun.map(item3 => {
+        domain.map(item4 => {
+          insertText += `
+          <li class='list-group-item'>
+          ${item}${item2}${item3}${item4}
+          </li>
+          `;
+        });
+      });
+    });
+  });
+
+  insertText += "</ul>";
+  document.querySelector("#insert").innerHTML = insertText;
 };
